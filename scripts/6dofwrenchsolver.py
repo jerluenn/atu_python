976f2fca4855457843eef4834b0f10c30b218a5a
jerluen <jerluen0@gmail.com>
2022-09-29 10:49:06 +0800

    updated 6dofwrenchsolver.py

--- scripts/6dofwrenchsolver.py
+++ scripts/6dofwrenchsolver.py
@@ -8,7 +8,7 @@ from std_msgs.msg import *
 
 import time
 import numpy as np
-import pyquaternion
+from pyquaternion import Quaternion as Quat
 
 import sys
 sys.path.insert(0, '../src')
@@ -23,11 +23,29 @@ class wrench_solver:
         self.num_atu = num_atu
         self.robot_dict = robot_dict
         self.friction_coefficient = friction_coefficient
-        self.pos_w_centroid = np.array([-self.robot_dict['tether_length']*0.8, 0.0, self.robot_dict['tether_length']*0.5, 1, 0, 0, 0])
-        self.pos_w_d = np.zeros((7, num_atu))
-        self.ref = np.zeros((14, num_atu))
+        self.pos_w_centroid = np.array([-self.robot_dict['tether_length']*0.5, 0.0, self.robot_dict['tether_length']*0.8, 1, 0, 0, 0])
+        self.pos_w_d = np.zeros((num_atu, 7))
+        self.ref = np.zeros((num_atu, 14))
+        self.wrench_list = np.zeros((num_atu, 6))
+        self.pos_centroid_d = pos_centroid_d
+        self.median_i = 0
+        self.median_pose = np.zeros((7, 7))
+        self.pos_w_centroid_optitrack = np.zeros(7)
+        self.quat = Quat([1, 0, 0, 0])
+        self.wrench_msg = WrenchStamped()
+
         self.initialise_inverse_solvers(pos_w_p)
         self.initialise_6dof_sensor(pos_centroid_d) 
+        self.initialiseNode()
+
+    def initialiseNode(self):
+
+        rospy.init_node('wrench_solver')
+
+        self.initialisePublishers()
+        self.initialiseSubscribers()
+        self.mainLoop()
+        rospy.spin()
 
     def initialise_inverse_solvers(self, pos_w_p): 
 
@@ -44,7 +62,7 @@ class wrench_solver:
 
             next_step_sol = np.array([0, 0, 0, 1, 0, 0, 0, 1.35202744e-01,  8.59444117e-11,  1.38997104e-01, -3.21851497e-11,  6.09179901e-03, -5.40886376e-12, 0])
             next_step_sol[0:7] = pos_w_p[i]
-            self.solver.set(0, 'x', next_step_sol)
+            self.inverse_solvers_list[i].set(0, 'x', next_step_sol)
 
             for k in range(robot_dict['integration_steps']): 
 
@@ -55,13 +73,14 @@ class wrench_solver:
 
     def initialise_6dof_sensor(self, pos_centroid_d):
 
-        NUM_ITERATIONS = 1000
+        NUM_ITERATIONS = 300
 
         for i in range(self.num_atu):
 
-            self.pos_w_d[0:3, i] = self.pos_w_centroid + pos_centroid_d[0:3, i]
-            self.ref[0:7, i] = self.pos_w_d[0:7, i]
-            self.inverse_solvers_list[i].set(self.ref[:, i])
+            self.pos_w_d[i, 0:3] = self.pos_w_centroid[0:3] + pos_centroid_d[i, 0:3]
+            self.pos_w_d[i, 3] = 1
+            self.ref[i, 0:7] = self.pos_w_d[i, 0:7]
+            self.inverse_solvers_list[i].set(self.robot_dict['integration_steps'], 'yref', self.ref[i, :])
 
             for k in range(NUM_ITERATIONS):
 
@@ -72,15 +91,67 @@ class wrench_solver:
 
         rospy.Subscriber('/relative_pose', PoseStamped, self.relative_pose_callback)
 
+    def initialisePublishers(self):
+
+        self.wrench_publisher = rospy.Publisher('/estimated_wrench', WrenchStamped, queue_size=1)
+
+    def publish_data(self): 
+
+        self.wrench_publisher.publish(self.wrench_msg)
+
     def solve_structure_problem(self): 
 
         pass 
 
-    def relative_pose_callback(self):
+    def update_solver(self):
 
-        pass 
+        for i in range(self.num_atu): 
 
-    
+            self.quat[0] = self.pos_w_centroid[3]
+            self.quat[1] = self.pos_w_centroid[4]
+            self.quat[2] = self.pos_w_centroid[5]
+            self.quat[3] = self.pos_w_centroid[6]
+            self.ref[i, 0:3] = self.pos_w_centroid[0:3] + self.quat.rotate(self.pos_centroid_d[i, 0:3])
+            self.inverse_solvers_list[i].set(self.robot_dict['integration_steps'], 'yref', self.ref[i, :])
+            self.inverse_solvers_list[i].solve()
+            self.wrench_list[i, :] = self.inverse_solvers_list[i].get(self.robot_dict['integration_steps'], 'x')[7:13]
+
+        self.solve_structure_problem()
+
+    def medianFilterStep(self): 
+
+        self.median_pose[self.median_i%7, :] = self.pos_w_centroid_optitrack
+        self.median_i += 1
+        self.pos_w_centroid = np.median(self.median_pose[:, 0]), np.median(self.median_pose[:, 1]), np.median(self.median_pose[:, 2]),\
+        np.median(self.median_pose[:, 3]), np.median(self.median_pose[:, 4]), np.median(self.median_pose[:, 5]), np.median(self.median_pose[:, 6])
+
+        
+
+    def relative_pose_callback(self, data):
+
+        self.pos_w_centroid_optitrack[0] = data.pose.position.x
+        self.pos_w_centroid_optitrack[1] = data.pose.position.y
+        self.pos_w_centroid_optitrack[2] = data.pose.position.z
+        self.pos_w_centroid_optitrack[3] = np.abs(data.pose.orientation.w)
+        self.pos_w_centroid_optitrack[4] = data.pose.orientation.x
+        self.pos_w_centroid_optitrack[5] = data.pose.orientation.y
+        self.pos_w_centroid_optitrack[6] = data.pose.orientation.z
+
+        self.medianFilterStep()
+
+    def mainLoop(self): 
+
+        while not rospy.is_shutdown(): 
+
+            t0 = time.time()
+
+            self.update_solver()
+            self.publish_data()
+
+            time.sleep(0.0001) 
+
+            print(f"Total time (s): {time.time() - t0}")
+
 if __name__ == "__main__":
 
     robot_dict = {}
